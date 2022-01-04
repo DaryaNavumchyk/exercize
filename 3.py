3d394051dd27d1a1d86ed94a05ab24fac5b0f3fe
Darya <75693374+DaryaNavumchyk@users.noreply.github.com>
2022-01-04 22:13:43 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,8 +1,14 @@
 
 list1 = [1, 2, 3]
-list2 = ["a", "b", "c"]
+list2 = ['a', 'b', 'c']
 
-result = [None]*(len(list1)+len(list2))
-result[::2] = list1
-result[1::2] = list2[0]
-print(result)
+#result = [None]*(len(list1)+len(list2))
+#result[::2] = list1
+#result[1::2] = list2
+#print(result)
+
+res=list(map(list, zip(list1, list2)))
+print(res)
+
+print(list1)
+print(list2)
