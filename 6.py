76a09d79890bcd5c0705a326063a214f3c4959f0
Darya <75693374+DaryaNavumchyk@users.noreply.github.com>
2022-01-04 22:01:14 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,8 +3,8 @@ list = [1, 2, 3, 4, 5, 6, 7]
 #list.reverse()
 #print(list)
 
-for x in reversed(list): 
-    print(x)
+#for x in reversed(list): 
+#    print(x)
 
-list_r=list[::-1]
-print(list_r)
+#list_r=list[::-1]
+#print(list_r)
