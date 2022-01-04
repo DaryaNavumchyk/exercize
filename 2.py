dec39b18e40a90a054623587bdf233cb2e810369
Darya <75693374+DaryaNavumchyk@users.noreply.github.com>
2022-01-04 23:31:55 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,8 +1,17 @@
-def fibonacci(n):
-    a, b = 1, 1
-    for i in range(n):
-        yield a
-        a, b = b, a + b
+#def fibonacci(n):
+#    a, b = 1, 1
+#    for i in range(n):
+#        yield a
+#        a, b = b, a + b
+#data = list(fibonacci(100))
+#print(data)
 
-data = list(fibonacci(100))
-print(data)
+
+
+fibonacci1 = fibonacci2 = 1
+print(fibonacci1, fibonacci2, end=' ')
+ 
+for i in range(100):
+    fibonacci1, fibonacci2 = fibonacci2, fibonacci1 + fibonacci2
+    print(fibonacci2, end=' ')
+ 
