9b8a047732c8424afef0a5fd6e1b46c5dda4bc65
Darya <75693374+DaryaNavumchyk@users.noreply.github.com>
2022-01-06 00:05:33 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -8,3 +8,12 @@ list = [1, 2, 3, 4, 5, 6, 7]
 
 #list_r=list[::-1]
 #print(list_r)
+
+
+ 
+def h(liss):
+    for i in range(len(liss)-1, -1, -1):
+        list_f.append(liss[i])
+    return list_f     
+
+print(h(liss))
